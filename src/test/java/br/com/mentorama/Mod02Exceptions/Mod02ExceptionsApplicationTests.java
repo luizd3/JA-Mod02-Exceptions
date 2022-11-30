2be0c6cc8e4373f989aed1ee78730aae68f7736f
Luiz Dorça <luizdorca@gmail.com>
2022-11-29 21:17:42 -0300

    Commit inicial

---
+++ src/test/java/br/com/mentorama/Mod02Exceptions/Mod02ExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mentorama.Mod02Exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod02ExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
