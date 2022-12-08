d441c5e2b1817bd226100cecffc100a9fe0f91c2
Luiz Dorça <luizdorca@gmail.com>
2022-12-07 21:11:09 -0300

    Usando if else para tratar exceções

--- src/main/java/br/com/mentorama/Mod02Exceptions/AlunosMain.java
+++ src/main/java/br/com/mentorama/Mod02Exceptions/AlunosMain.java
@@ -3,9 +3,20 @@ package br.com.mentorama.Mod02Exceptions;
 public class AlunosMain {
 
 	public static void main(String[] args) {
-		AlunoService alunoService = new AlunoService();
-		alunoService.findAll().stream().forEach(System.out::println);
-		System.out.println("SUCESSO");
+//		AlunoService alunoService = new AlunoService();
+//		alunoService.findAll().stream().forEach(System.out::println);
+//		System.out.println("SUCESSO");
+
+//		String stringX = null;
+//		String stringX = args[3];
+
+		if (args.length > 0) {
+			String stringX = args[0];
+			System.out.println(stringX.contains("x"));
+		} else {
+			System.out.println("Não tem nada");
+		}
+
 	}
 
 }
